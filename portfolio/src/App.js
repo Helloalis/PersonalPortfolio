6c3337c2191b64431879a5dee23782dde7731903
Helloalis <rohanrtkurup@gmail.com>
2022-02-17 12:01:00 -0500

    Navbar added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,15 +1,39 @@
 import './App.css';
+import React, { Component } from 'react';
+
+function Intro(props) {
+    return <div><h1>Introduction</h1> <p>Hello! My name is Rohan Kurup. I am currently in the Collabera Jump training program to be a Java Developer</p></div>
+}
+
 
 function App() {
 return (
-    <div className="App">
+    <div class="row App">
         <header>
-            <div className="App-header-content"></div>
-            <div className="App-header-bar"></div>
+            <div class="row App-header-content" min-height="8.5vh"> </div> 
+            <nav class="row navbar navbar-expand-sm App-header-bar">
+                <div class="container-fluid">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="#intro" class="nav-link App-link">Introduction</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#portfolio" class="nav-link App-link">Portfolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#about" class="nav-link App-link">About me</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#contact" class="nav-link App-link">Contact me</a>
+                         </li>
+                    </ul>
+                </div>
+
+            </nav>
         </header>
+        <br/>
         <div className="App-main">
-            <p>Edit <code>src/App.js</code> and save to reload.</p>
-            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >Learn React</a>
+            <div id="intro"><Intro /></div>
         </div>
         <footer className="App-footer">
             <hr className="footerDivider" />
