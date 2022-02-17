c45012dd67b840883779f7f05b4a6d2589157224
Helloalis <rohanrtkurup@gmail.com>
2022-02-17 10:39:16 -0500

    February 17 update

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>
+            <div className="App-header-content"></div>
+            <div className="App-header-bar"></div>
+        </header>
+        <div className="App-main">
+            <p>Edit <code>src/App.js</code> and save to reload.</p>
+            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >Learn React</a>
+        </div>
+        <footer className="App-footer">
+            <hr className="footerDivider" />
+            <p>Email me at <a href="mailto:rohanrtkurup@gmail.com" className="App-link">rohanrtkurup@gmail.com</a></p>
+        </footer>
+
     </div>
   );
 }
