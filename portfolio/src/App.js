96681e5acfd9d06d11f9a77dd473b92d515798ed
Helloalis <rohanrtkurup@gmail.com>
2022-02-18 09:01:16 -0500

    Most functionality added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,43 +1,131 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import React, { Component } from 'react';
+import React, { useState  } from 'react';
 import Contact from "./components/Contact.js";
+import passChe from "./images/passwordChecker.png";
+import passCheOu from "./images/passwordCheckerOutput.png";
+import ll from "./images/linkedList.png";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+
 
 function Intro(props) {
-    return <div><h1>Introduction</h1> <p>Hello! My name is []. I am currently in the [] training program to be a Java Developer</p></div>
+    return <div><h1>Introduction</h1> <p>Hello! My name is Rohan. I am currently in the Collabera training program to be a Java Developer</p></div>
 }
 
+function PasswordChecker() {
+	return (
+		<div class="row passwordChecker">
+            <img src={passChe} class="col-4 rounded float-start" alt="This program evaluates password strengths" />
+            <div class="col-4">
+                <p>This program evaluates password strengths. It test whether passwords match. It uses 5 criteria: </p>
+                <ul class="list-group">
+                    <li class="list-group-item list-group-item-dark">Length has to be greater than 6, and a strong one has to be longer than 10</li>
+                    <li class="list-group-item list-group-item-dark">Has to contain at least one uppercase letter</li>
+                    <li class="list-group-item list-group-item-dark">Has to contain at least one lower case letter</li>
+                    <li class="list-group-item list-group-item-dark">Has to contain one number</li>
+                    <li class="list-group-item list-group-item-dark" >Cannot have more than 2 of the same character in a row</li>
+                </ul>
+            </div>
+            <img src={passCheOu} class="col-4 rounded float-end" alt="This program evaluates password strengths" />
+		</div>
+	)
+}
+
+function LinkedList() {
+    return (
+    <div class="row linkedList">
+            <img class="col-4" src={ll} alt="The linked list interface" />
+            <div class="col">
+                <p> This was an assignment for college. We were asked to is to write a generic double singly-linked list class with an iterator, and a generic sorted double singly-linked list class with an iterator that inherits from your generic double singly-linked list class.</p>
+            </div>
+    </div>
+    )
+}
+
+function Home() {
+    return <h3>Click a link to see one of the projects</h3>
+}
+
+function bookTracker() {
+    return (
+        
+    )
+}
+
+function Layout(){
+    return (
+        <div class="portfolio">
+            <h2>Portfolio</h2>
+            <p>Heres my portfolio</p>
+            <nav>
+                <ul>
+                    <li><Link class="App-Link" to="/">Home</Link></li>
+                    <li><Link class="App-Link" to="/linkedList">LinkedList</Link></li>
+                    <li><Link class="App-Link" to="/passwordChecker">Password Checker</Link></li>
+                </ul>
+            </nav>
+            <Outlet />
+        </div>
+    )
+};
 
 function App() {
+
+    const [style, setStyle] = useState("App-def");
+
+    const changeStyle = () => {
+        console.log("you just clicked");
+
+        setStyle("App-alt");
+    };
 return (
-    <div class="row App">
+    <div class={style + " row"}>
         <header>
-            <div class="row App-header-content" min-height="8.5vh"> </div> 
-            <nav class="row navbar navbar-expand-sm App-header-bar">
+            <div class="row App-header-content-def" min-height="8.5vh"> </div> 
+            <nav class="row navbar navbar-expand-sm App-header-bar-def">
                 <div class="container-fluid">
-                    <ul class="navbar-nav">
+                    <ul class="navbar-nav" id="navBare">
                         <li class="nav-item"><a href="#intro" class="nav-link App-link">Introduction</a></li>
                         <li class="nav-item"><a href="#portfolio" class="nav-link App-link">Portfolio</a></li>
-                        <li class="nav-item"><a href="#about" class="nav-link App-link">About me</a></li>
+                        {/*<li class="nav-item"><a href="#about" class="nav-link App-link">About me</a></li> */}
                         <li class="nav-item"><a href="#contact" class="nav-link App-link">Contact me</a></li>
+                        <li class="nav-item"><a href="#navBares" class="nav-link App-link" onClick={changeStyle}>Toggle Color scheme</a></li>
                     </ul>
                 </div>
             </nav>
         </header>
 
 
-        <br/>
-        <div className="App-main">
-            <div id="intro"><Intro /></div>
+        
+        <div className="App-main-def">
+            <br />
+            <div id="intro"><Intro /></div><br />
+
+            <div id="portfolio">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Layout />}>
+                            <Route index element={<Home />} />
+                            <Route path="linkedList" element={<LinkedList />} />
+                            <Route path="passwordChecker" element={<PasswordChecker />} />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+
+                
+            </div><br />
+
             <div id="contact">
                 <p>Email me at <a href="mailto:rohanrtkurup@gmail.com" className="App-link">rohanrtkurup@gmail.com</a></p>
                 <p>Or, use this form:</p>
-                <Contact /></div>
+                <Contact />
+            </div>
 
 
         </div>
 
-        <footer className="App-footer">
+        <footer className="App-footer-def">
             <hr className="footerDivider" />
             
         </footer>
--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,6 +1,10 @@
 import './../App.css';
-import React, { useRef } from 'react';
+import React, { Component, useRef } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import emailjs from '@emailjs/browser';
+
+
+
 
 class Contact extends React.Component {
     constructor(props) {
@@ -9,20 +13,12 @@ class Contact extends React.Component {
             
             from_name: '',
             sender_email: '',
-            message: ''
+            message: '',
+            sentSuccess: false,
+            emailSent: false
+            
         };
-    }
-
-    componentDidMount() {
-        this.setState({message: "Hello! Enter your message here"});
-        this.setState({name: "Your name goes here" });
-        this.setState({ sender_email: "youremail@email.com" });
-    }
-
-    componentDidUpdate() {
-        this.setState({ message: "Hello! Enter your message here" });
-        this.setState({ name: "Your name goes here" });
-        this.setState({ sender_email: "youremail@email.com" });
+        {/*emailSent Measures whether an email has attempted to send, sentSuccess mesasures wether it worked*/ }
     }
 
     // saves the user's name entered to state
@@ -32,17 +28,20 @@ class Contact extends React.Component {
 
     // saves the user's email entered to state
     emailChange = (event) => {
-        this.setState({ email: event.target.value })
+        this.setState({ sender_email: event.target.value })
     }
 
     // saves the user's message entered to state
     messageChange = (event) => {
-        this.setState({ feedback: event.target.value })
+        this.setState({ message: event.target.value })
     }
+    
 
     //onSubmit of email form
     handleSubmit = (event) => {
         event.preventDefault();
+
+
 
         //This templateId is created in EmailJS.com
         const templateId = 'template_37s5ljk';
@@ -51,25 +50,57 @@ class Contact extends React.Component {
         //from the form and sends the email with the information gathered 
         //and formats the email based on the templateID provided.
         this.sendFeedback(templateId, {
-            message: this.state.feedback,
+            message: this.state.message,
             name: this.state.name,
-            email: this.state.email
+            sender_email: this.state.sender_email
         }
         )
     }
+
+    
 
     //Custom EmailJS method
     sendFeedback = (templateId, variables) => {
         window.emailjs.send("service_cg9yyfk", templateId,variables).then(res => {
             // Email successfully sent alert
-            alert("Email Successfully Sent");
+            this.setState({ emailSent: true });
+            this.setState({ sentSuccess: true });
+            console.log("Hello");
+            
         })
 
         // Email Failed to send Error alert
         .catch(err => {
-            alert("Email Failed to Send");
+            this.setState({ emailSent: true });
+            this.setState({ sentSuccess: false });
             console.error('Email Error:', err)
         })
+    }
+
+    componentDidUpdate() {
+        let currentDate = new Date();
+        let time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+        let currState = "Email Sent: " + this.state.emailSent + "; Sent Success: " + this.state.sentSuccess;
+        console.log(time + ": " + currState);
+        if (this.state.emailSent && this.state.sentSuccess) {
+            console.log("Success notification added");
+        }
+        else if (this.state.emailSent && this.state.sentSuccess === false) {
+            console.log("Email failed to send, warning added");
+        }
+    }
+
+    componentDidMount() {
+        console.log("EmailSent Mounted");
+        const es = this.state.emailSent;
+        const ss = this.state.sentSuccess;
+        this.setState({ sentSuccess: ss });
+        this.setState({ emailSent: es });
+    }
+
+    componentWillUnmount() {
+        this.setState({ emailSent: false });
+        this.setState({ sentSuccess: false });
     }
 
     render() {
@@ -77,21 +108,30 @@ class Contact extends React.Component {
             <div class="form">
                 <form id="form" onSubmit={this.handleSubmit}>
                     <div class="field">
-                        <label class="form-label" for="from_name">Your Name</label>
-                        <input type="text" name="from_name" id="from_name" class="form-control" />
+                        <label class="form-label" for="name">Your Name</label>
+                        <input type="text" name="name" id="name" class="form-control" onChange={this.nameChange}/>
                     </div>
                     <div class="field">
                         <label class="form-label" for="sender_email">Your Email</label>
-                        <input type="text" name="sender_email" id="sender_email" class="form-control" />
+                        <input type="text" name="sender_email" id="sender_email" class="form-control" onChange={this.emailChange} />
                     </div>
                     <div class="field">
                         <label class="form-label" for="message">Your Message</label>
-                        <input type="text" name="message" id="message" class="form-control" />
+                        <input type="text" name="message" id="message" class="form-control" onChange={this.messageChange} />
                     </div>
                     <input type="submit" id="button" value="Send Email" class="form-control" />
-                </form></div>
+                </form>
+
+                {this.state.emailSent && this.state.sentSuccess && <div class="alert alert-success alert-dismissible fade show" role="alert"><strong>Success!</strong> Email Sent<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>}
+                {this.state.emailSent && this.state.sentSuccess === false && <div class="alert alert-warning alert-dismissible fade show" role="alert"><strong>Warning!</strong> Indicates a warning that might need attention.<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>}
+            </div>
         );
     }
 };
+
+
+        
+
+
 /*//https://medium.com/@skinsfannick/implementing-emailjs-in-react-300989edf380 */
 export default Contact;
